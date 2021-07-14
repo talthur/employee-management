a8adfacc99dad0d16f19fb0f8cfb216358ea37cc
talthur <talthur.ga@gmail.com>
2021-07-14 18:22:50 -0300

    Creating project

---
+++ src/test/java/br/com/talthur/employeemanagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.talthur.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
